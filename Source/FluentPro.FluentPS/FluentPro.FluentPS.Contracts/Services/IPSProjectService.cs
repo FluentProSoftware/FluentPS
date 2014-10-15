2d292264fb56b59aeb4d32c13c1bb0f97ccd8bdc
Alexey Udovichenko <alexey.udovichenko@riafx.com>
2014-10-15 23:10:48 +0300

    Added project service stub.

---
+++ Source/FluentPro.FluentPS/FluentPro.FluentPS.Contracts/Services/IPSProjectService.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentPro.FluentPS.Contracts.Services
+{
+    using System.Collections.Generic;
+
+    public interface IPSProjectService<TProject>
+    {
+        IEnumerable<TProject> GetAll();
+    }
+}
---
+++ Source/FluentPro.FluentPS/FluentPro.FluentPS/Services/PSProjectService.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentPro.FluentPS.Services
+{
+    using FluentPro.FluentPS.Contracts.Services;
+    using System.Collections.Generic;
+
+    public class PSProjectService<TProject> : IPSProjectService<TProject>
+    {
+        public IEnumerable<TProject> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
