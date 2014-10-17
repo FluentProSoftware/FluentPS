13c7f2256d37816bcdb7f935f7cf4e32da1566a9
Alexey Udovichenko <alexey.udovichenko@riafx.com>
2014-10-17 13:54:49 +0300

    Added project wcf proxy, created few interfaces for wrapper

---
+++ Source/FluentPro.FluentPS/FluentPro.FluentPS.Contracts/Proxies/IProjectWcfService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentPro.FluentPS.Contracts.Proxies
+{
+    public interface IProjectWcfService
+    {
+    }
+}
---
+++ Source/FluentPro.FluentPS/FluentPro.FluentPS.Proxy/Services/ProjectWcfService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentPro.FluentPS.Proxy.Services
+{
+    public class ProjectWcfService
+    {
+    }
+}
