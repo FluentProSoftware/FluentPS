7e0566f292732372a088c2405620a02ec17e49dc
boades <boades.net@gmail.com>
2014-11-17 23:12:18 +0200

    Added one more property name converter.

---
+++ Source/FluentPro.FluentPS/FluentPro.FluentPS.Common.Tests/Mapper/Converters/PropertyNameConverters/GetOriginalNamePropertyNameConverterTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentPro.FluentPS.Common.Mapper.Converters.PropertyNameConverters;
+using FluentPro.FluentPS.Common.Mapper.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentPro.FluentPS.Common.Tests.Mapper.Converters.PropertyNameConverters
+{
+    [TestClass]
+    public class GetOriginalNamePropertyNameConverterTests : PropertyNameConverterTests
+    {
+        [TestMethod]
+        public void GetName_Name_Returns_Name()
+        {
+            Validate("Name", "Name");
+        }
+
+        public override IPropertyNameConverter GetSut()
+        {
+            return new GetOriginalNamePropertyNameConverter();
+        }
+    }
+}
---
+++ Source/FluentPro.FluentPS/FluentPro.FluentPS.Common/Mapper/Converters/PropertyNameConverters/GetOriginalNamePropertyNameConverter.cs
@@ -0,0 +1,12 @@
+﻿using FluentPro.FluentPS.Common.Mapper.Interfaces;
+
+namespace FluentPro.FluentPS.Common.Mapper.Converters.PropertyNameConverters
+{
+    public class GetOriginalNamePropertyNameConverter : IPropertyNameConverter
+    {
+        public string GetName(string sourceName)
+        {
+            return sourceName;
+        }
+    }
+}
